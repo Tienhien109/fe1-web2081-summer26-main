9f2fe6fd4c5f6a2792dfebc3cc5d90310900aa40
Tienhien109 <you@example.com>
2026-07-13 19:22:55 +0700

    Update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { Stories } from './pages/stories/stories';
+import { AddStory } from './pages/add-story/add-story';
 
 export const routes: Routes = [
   {
@@ -9,5 +10,9 @@ export const routes: Routes = [
   {
     path: 'stories',
     component: Stories,
+  },
+  {
+    path: 'add-story',
+    component: AddStory,
   },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,6 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-
-}
+export class App {}
