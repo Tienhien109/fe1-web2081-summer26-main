287a6af222bccc2a853f7a1c87e906f8cf8e60f3
Tienhien109 <you@example.com>
2026-07-31 19:30:41 +0700

    bai1-2lab8

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,62 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  loginForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {
+    if (localStorage.getItem('token')) {
+      this.router.navigate(['/']);
+    }
+
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  submit() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    const { email, password } = this.loginForm.value;
+
+    this.http
+      .get<any[]>(
+        `http://localhost:3000/users?email=${email}&password=${password}`
+      )
+      .subscribe((res) => {
+        if (res.length > 0) {
+          localStorage.setItem('token', 'fake-token');
+          localStorage.setItem('user', JSON.stringify(res[0]));
+
+          alert('Đăng nhập thành công');
+
+          this.router.navigate(['/']);
+        } else {
+          alert('Sai email hoặc mật khẩu');
+        }
+      });
+  }
 }
