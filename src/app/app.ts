eb97b960a47ce96b9688735082df39082c729c2f
Tienhien109 <you@example.com>
2026-07-31 19:31:47 +0700

    bai34lab8

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,26 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {}
+export class App implements OnInit {
+
+  user: any = null;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    const data = localStorage.getItem('user');
+
+    if (data) {
+      this.user = JSON.parse(data);
+    }
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+
+    this.user = null;
+
+    this.router.navigate(['/login']);
+  }
+}
